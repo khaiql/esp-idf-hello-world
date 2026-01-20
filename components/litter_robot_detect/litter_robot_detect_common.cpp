b5ea05669933a7b00034ac36068bffd6bdd13048
Scott Le <sle@atlassian.com>
2026-01-20 16:52:46 +1100

    add extern C

--- components/litter_robot_detect/litter_robot_detect_common.cpp
+++ components/litter_robot_detect/litter_robot_detect_common.cpp
@@ -1,17 +1,20 @@
 #include "litter_robot_detect.hpp"
 
-extern const uint8_t test_nachi_jpg_start[] asm("_binary_test_image_jpg_start");
-extern const uint8_t test_nachi_jpg_end[] asm("_binary_test_image_jpg_end");
+extern "C"
+{
+    extern const uint8_t _binary_test_image_jpg_start[] asm("_binary_test_image_jpg_start");
+    extern const uint8_t _binary_test_image_jpg_end[] asm("_binary_test_image_jpg_end");
+}
 
 void litter_robot_detect::CatDetect::test_model()
 {
     ESP_LOGI(TAG, "Testing model with embedded image");
 
-    size_t test_image_len = test_nachi_jpg_end - test_nachi_jpg_start;
+    size_t test_image_len = _binary_test_image_jpg_end - _binary_test_image_jpg_start;
 
     // Create a dummy camera frame buffer
     camera_fb_t fb;
-    fb.buf = (uint8_t *)test_nachi_jpg_start;
+    fb.buf = (uint8_t *)_binary_test_image_jpg_start;
     fb.len = test_image_len;
     fb.width = 0;  // Decoder might determine this, or set if known/needed
     fb.height = 0; // Decoder might determine this, or set if known/needed
